272d7c96462f908551884df61ac76934690a7f79
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-07-21 14:49:02 +0300

    Done.

--- src/Clock.js
+++ src/Clock.js
@@ -3,10 +3,16 @@ import React, { useState, useEffect } from "react";
 function Clock() {
   const [time, setTime] = useState(new Date());
 
-  useEffect(() => {
+  const timerID=useEffect(() => {
     setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+   // returning a cleanup function incase of unmount of clock component
+   return function cleanup() {
+    clearInterval(timerID);
+  };
+
   }, []);
 
   return <div>{time.toString()}</div>;
